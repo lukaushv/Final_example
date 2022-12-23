73e7a91e724cfcb6d2c9e7bde77e6970de809029
lukaushv <luka.ushveridze@gmail.com>
2022-12-23 14:40:08 +0400

    Add project files.

---
+++ Final_example/Algorithms.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Final_example
+{
+    public class Algorithms
+    {
+
+        public static string RemoveConsonant(string word)
+        {
+            string result = Regex.Replace(word, @"[^aeiouyAEIOUY]", "");
+
+            return result;
+        }
+
+        public static string RemoveVowel(string word)
+        {
+            string result = Regex.Replace(word, @"[aeiouyAEIOUY]", "");
+
+            return result;
+        }
+        
+        public static string LongestInSentence(string word)
+        {
+            string[] words = word.Split(new[] { " " }, StringSplitOptions.None);
+            string result = string.Empty;
+            int len = 0;
+            foreach(string s in words)
+            {
+                if(s.Length > len)
+                {
+                    result= s;
+                    len = s.Length;
+                }
+            }
+
+            return result;
+        }
+
+        public static string ShortestInSentence(string word)
+        {
+            string[] words = word.Split(new[] { " " }, StringSplitOptions.None);
+            string result = string.Empty;
+            int len = 0;
+            foreach (string s in words)
+            {
+                if (s.Length > len)
+                {
+                    result = s;
+                    len = s.Length;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Final_example/Program.cs
@@ -0,0 +1,17 @@
+﻿using Final_example;
+
+string sentence = "where are you";
+string sentence1 = "nitsa nikolaishvili";
+string sentence2 = "ormocdacameti sitkva";
+string sentence3 = "atas as ocda erti";
+string sentence4 = "Bankai getsuga tenshou";
+
+Console.WriteLine(Algorithms.LongestInSentence(sentence));
+Console.WriteLine(Algorithms.LongestInSentence(sentence1));
+Console.WriteLine(Algorithms.LongestInSentence(sentence2));
+Console.WriteLine(Algorithms.LongestInSentence(sentence3));
+Console.WriteLine(Algorithms.LongestInSentence(sentence4));
+
+
+
+
